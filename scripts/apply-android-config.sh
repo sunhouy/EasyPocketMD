cb3d5eae1c78d819956d67b550e5f05028524aec
Houyun Sun <18763177732@139.com>
2026-04-15 11:51:15 +0000

    状态栏终极修复

--- scripts/apply-android-config.sh
+++ scripts/apply-android-config.sh
@@ -105,7 +105,6 @@ package $ANDROID_PACKAGE_NAME
 
 import android.os.Bundle
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 
@@ -113,17 +112,15 @@ class MainActivity : TauriActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 保持状态栏非沉浸布局，让内容不绘制到状态栏后方。
-        WindowCompat.setDecorFitsSystemWindows(window, true)
-
         val decorView = window.decorView
         val rootView = decorView.findViewById<android.view.View>(android.R.id.content)
 
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
             val imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 
-            view.updatePadding(
+            v.updatePadding(
+                top = systemBars.top,
                 bottom = if (imeInsets.bottom > 0) imeInsets.bottom else systemBars.bottom
             )
             insets
